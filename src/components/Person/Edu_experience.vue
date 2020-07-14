cf88a5ce1dbf1d88d3fb88f469de0e566cf537f1
Jay <zhi-jie.wang@mail.foxconn.com>
2020-07-14 18:25:46 +0800

    Feat: 新增教育经历页

---
+++ src/components/Person/Edu_experience.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="tableContain">
+        <div class="table_tool">
+            <el-button type="primary"  icon="el-icon-plus">新增一笔</el-button>
+        </div>
+        <el-table :data="tableData" stripe border style="width: 100%" highlight-current-row="true">
+            <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+            <el-table-column prop="date" label="日期" sortable width="180">
+                <template slot-scope="scope">
+                    <i class="el-icon-time"></i>
+                    <span style="margin-left: 10px">{{ scope.row.date }}</span>
+                </template>
+            </el-table-column>
+            <el-table-column prop="address" label="地址"></el-table-column>
+            <el-table-column fixed="right" label="操作" width="140">
+                <template slot-scope="scope">
+                    <el-button
+                        @click.native.prevent="deleteRow(scope.$index, tableData)"
+                        icon="el-icon-view"
+                        circle
+                        size="small"
+                        title="查看"
+                    ></el-button>
+                    <el-button
+                        @click.native.prevent="deleteRow(scope.$index, tableData)"
+                        type="primary"
+                        icon="el-icon-edit"
+                        circle
+                        size="small"
+                        title="编辑"
+                    ></el-button>
+                    <el-button
+                        @click.native.prevent="deleteRow(scope.$index, tableData)"
+                        type="danger"
+                        icon="el-icon-delete"
+                        circle
+                        size="small"
+                        title="移除"
+                    ></el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            tableData: [
+                {
+                    date: "2016-05-02",
+                    name: "王小虎",
+                    address: "上海市普陀区金沙江路 1518 弄"
+                },
+                {
+                    date: "2016-05-04",
+                    name: "王小虎",
+                    address: "上海市普陀区金沙江路 1517 弄"
+                },
+                {
+                    date: "2016-05-01",
+                    name: "王小虎",
+                    address: "上海市普陀区金沙江路 1519 弄"
+                },
+                {
+                    date: "2016-05-03",
+                    name: "王小虎",
+                    address: "上海市普陀区金沙江路 1516 弄"
+                }
+            ]
+        };
+    }
+};
+</script>
+
+<style scoped lang="less">
+.tableContain {
+    max-width: 1200px;
+    margin-left: 60px;
+    .table_tool {
+        margin-bottom: 20px;
+        padding-right: 25px;
+        text-align: right;
+    }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,42 +4,45 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-    redirect:'PersonalInfo',
-    children:[
-        {
-          path: '/test',
-          name: '测试页面',
-          component: () => import(/* webpackChunkName: "test" */ '@c/Test.vue')
-        },
-        {
-          path: '/tableView',
-          name: '表格页面',
-          component: () => import(/* webpackChunkName: "test" */ '../components/TableView.vue')
-        },
-        {
-            path: '/PersonalInfo',
-            name: '个人基础信息',
-            component: () => import(/* webpackChunkName: "PersonalInfo" */ '../components/Person/PersonalInfo.vue')
-          },
-    ]
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home,
+        redirect: 'edu_experience',
+        children: [{
+                path: '/test',
+                name: '测试页面',
+                component: () => import( /* webpackChunkName: "test" */ '@c/Test.vue')
+            },
+            {
+                path: '/tableView',
+                name: '表格页面',
+                component: () => import( /* webpackChunkName: "test" */ '../components/TableView.vue')
+            },
+            {
+                path: '/personalInfo',
+                name: '个人基础信息',
+                component: () => import( /* webpackChunkName: "PersonalInfo" */ '../components/Person/PersonalInfo.vue')
+            },
+            {
+                path: '/edu_experience',
+                name: '教育经历',
+                component: () => import( /* webpackChunkName: "edu_experience" */ '../components/Person/Edu_experience.vue')
+            },
+        ]
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import( /* webpackChunkName: "about" */ '../views/About.vue')
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
