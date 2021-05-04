ee4459d6687882a2d9daeb837aafabd0d2cfd9e3
Jay <1356573671@qq.com>
2021-05-04 19:45:00 +0800

    feat: 代码整理

--- src/router/index.js
+++ src/router/index.js
@@ -23,9 +23,6 @@ const routes = [
     meta: {
       auth: 3
     },
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
   },
   {
@@ -71,9 +68,6 @@ const routes = [
       meta: {
         auth: 0
       },
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "accountManagement" */ '../components/AccountManagement/index.vue')
     },
     {
@@ -113,9 +107,6 @@ const routes = [
     meta: {
       auth: 0
     },
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
@@ -124,9 +115,6 @@ const routes = [
     meta: {
       auth: 3
     },
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "404" */ '../views/404.vue')
   }
 ]
