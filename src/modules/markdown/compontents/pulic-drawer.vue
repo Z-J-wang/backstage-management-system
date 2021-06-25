93e5e5fffc448114eaced76b16e36bf03d83625f
Jay <zhi-jie.wang@mail.foxconn.com>
2021-06-25 15:24:09 +0800

    feat: pulic-drawer 优化

--- src/modules/markdown/compontents/pulic-drawer.vue
+++ src/modules/markdown/compontents/pulic-drawer.vue
@@ -1,31 +1,53 @@
 <template>
   <el-drawer title="完善文章信息" :visible.sync="drawer" direction="rtl" @closed="handleClose">
-    <el-form v-model="pulicData" ref="pulicData" :rules="pulicDataRule" label-width="100px">
-      <el-form-item label="文章标签：" prop="type">
+    <el-form :model="pulicData" ref="pulicData" :rules="pulicDataRule" label-width="100px">
+      <el-form-item label="文章标签：" prop="tags">
         <el-tag
-          :key="tag"
           v-for="(tag,i) in pulicData.tags"
+          :key="i"
           closable
           :disable-transitions="false"
           @close="delTag(i)"
         >{{tag}}</el-tag>
-        <el-button class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
-        <div class="tag-contain">
+        <el-button
+          class="button-new-tag"
+          size="small"
+          @click="tagControlVisible = true"
+          v-show="4 - pulicData.tags.length"
+        >+ New Tag</el-button>
+        <div class="tag-contain" v-show="tagControlVisible">
           <p>可以添加标签: {{ 4 - pulicData.tags.length }} 个</p>
-          <el-input v-model="neweTag" size="small" placeholder="新增标签" @keyup.native.enter="addTag"></el-input>
+          <i class="close el-icon-close" @click="tagControlVisible = false"></i>
+          <el-input v-model="newTag" size="small" placeholder="新增标签" @keyup.native.enter="addTag"></el-input>
           <div>
             <el-tag
-              :key="tag"
               v-for="(tag,i) in allTags"
+              :key="i"
               :disable-transitions="false"
               @click="selecTag(i)"
             >{{tag}}</el-tag>
           </div>
         </div>
       </el-form-item>
-      <el-form-item label="分类：" prop="type">
-        <el-select></el-select>
+      <el-form-item label="文章分类：" prop="category">
+        <el-select v-model="pulicData.category">
+          <el-option v-for="item in categories" :key="item" :label="item" :value="item"></el-option>
+        </el-select>
       </el-form-item>
+      <el-form-item label="文章简介：" prop="introduction">
+        <el-input
+          type="textarea"
+          :autosize="{ minRows: 10, maxRows: 20}"
+          placeholder="请输入内容"
+          :show-word-limit="true"
+          maxlength="500"
+          v-model="pulicData.introduction"
+        ></el-input>
+      </el-form-item>
+      <div class="toolbar">
+        <el-button @click="handleClose">取消</el-button>
+        <el-button type="primary" @click="pulic()">发布</el-button>
+      </div>
     </el-form>
   </el-drawer>
 </template>
@@ -41,13 +63,22 @@ export default {
   },
   data() {
     return {
-      drawer: this.visible,
+      drawer: this.visible, // drawer 显示隐藏
       pulicData: {
-        tags: ['标签一', '标签一']
+        // 文章表单数据
+        tags: ['标签一', '标签一'],
+        category: '',
+        introduction: ''
       },
-      pulicDataRule: {},
-      neweTag: '',
-      allTags: ['标签一', '标签一', '标签一', '标签一', '标签一', '标签一', '标签一', '标签一']
+      pulicDataRule: {
+        // 表单验证规则
+        tags: [{ required: true, message: '文章标签不能为空', trigger: 'blur' }],
+        category: [{ required: true, message: '文章分类不能为空', trigger: 'blur' }]
+      },
+      categories: ['类别一', '类别二'], // 已存在的文章分类
+      newTag: '', // 新增标签
+      tagControlVisible: false, // 标签设置模块显示隐藏
+      allTags: ['标签一', '标签一', '标签一', '标签一', '标签一', '标签一', '标签一', '标签一'] // 已存在的标签
     };
   },
   watch: {
@@ -64,7 +95,8 @@ export default {
     },
     addTag() {
       if (4 - this.pulicData.tags.length) {
-        this.pulicData.tags.push(this.neweTag);
+        this.pulicData.tags.push(this.newTag);
+        this.newTag = '';
       } else {
         this.$message.warning('最多可以添加 4 个标签');
       }
@@ -75,6 +107,9 @@ export default {
       } else {
         this.$message.warning('最多可以添加 4 个标签');
       }
+    },
+    pulic() {
+      this.$refs.pulicData.validate();
     }
   }
 };
@@ -92,6 +127,7 @@ export default {
   margin-left: 10px;
 }
 .tag-contain {
+  position: relative;
   margin-top: 20px;
   padding: 15px;
   border: 1px solid #000;
@@ -102,9 +138,20 @@ export default {
     font-size: 14px;
     line-height: 14px;
   }
+  .close {
+    position: absolute;
+    top: 15px;
+    right: 15px;
+    cursor: pointer;
+  }
   .el-tag {
     margin: 0 5px;
     cursor: pointer;
   }
+}
+.toolbar {
+  position: absolute;
+  bottom: 30px;
+  right: 30px;
 }
 </style>
